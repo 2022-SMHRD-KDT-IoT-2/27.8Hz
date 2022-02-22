e5a7564ad3acc93159a0674afbdd81dbccbcc6aa
vipstard <vipstar_d@naver.com>
2022-02-22 16:10:09 +0900

    4번째 커밋

--- 27.8Hz/src/main/java/test/test01.java
+++ 27.8Hz/src/main/java/test/test01.java
@@ -8,6 +8,14 @@ public class test01 {
 		System.out.println("2번째 커밋");
 		System.out.println("3번째 커밋");
 		
+		
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");    
+		
 		// 커밋 -> 로컬 레퍼지토리에 저장하는 작업!
 
 	}
