ab60ca7f02e446af1054631b92b44a5ebf03776f
vipstard <vipstar_d@naver.com>
2022-02-22 16:57:26 +0900

    동일

--- 27.8Hz/src/main/java/test/test05_동일.java
+++ 27.8Hz/src/main/java/test/test05_동일.java
@@ -4,7 +4,11 @@ public class test05_µø¿œ {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
 	}
 
 }
