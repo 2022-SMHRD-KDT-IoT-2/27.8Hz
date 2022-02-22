4f550e1a4aac6435755033efa8b353fccd0c4bec
smhrd <smhrd@59.0.124.112>
2022-02-22 16:38:43 +0900

    현빈짱

--- 27.8Hz/src/main/java/test/test01.java
+++ 27.8Hz/src/main/java/test/test01.java
@@ -21,6 +21,7 @@ public class test01 {
 		System.out.println("3. 영석 커밋");
 		System.out.println("4. 현빈 커밋");
 		System.out.println("5. 동일 커밋");
+		System.out.println("6. 옥현빈 커밋");
 		
 		
 		// 커밋 -> 로컬 레퍼지토리에 저장하는 작업!
