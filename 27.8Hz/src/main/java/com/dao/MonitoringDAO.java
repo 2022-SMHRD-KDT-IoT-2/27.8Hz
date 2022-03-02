f963a87a56ca04ea6b007b26ba404a8590374dd1
smhrd <smhrd@59.0.236.167>
2022-03-02 15:21:13 +0900

    monitoringDAO com.dao에 옮김 : 소원

--- 27.8Hz/src/main/java/com/dao/MonitoringDAO.java
+++ 27.8Hz/src/main/java/com/dao/MonitoringDAO.java
@@ -1,10 +1,12 @@
-package com.model;
+package com.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+
+import com.model.MonitoringVO;
 
 public class MonitoringDAO {
 
