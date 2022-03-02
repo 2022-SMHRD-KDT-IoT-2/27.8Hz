c2fc05c330be605a8496e0f51c71db644ed0aac5
smhrd <smhrd@59.0.236.167>
2022-03-02 09:37:04 +0900

    monitoringVO 수정 + monitoringDAO 추가 (소원)

---
+++ 27.8Hz/src/main/java/com/model/MonitoringDAO.java
@@ -0,0 +1,78 @@
+package com.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+public class MonitoringDAO {
+
+	Connection conn = null;
+	PreparedStatement pst = null;
+	ResultSet rs = null ;
+
+	public void connect() {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String user = "campus_d_1_0216";
+			String password = "smhrd1";
+
+			conn = DriverManager.getConnection(url, user, password);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	} // end of connect
+
+	public void close() {
+		try {
+			if(rs!=null) {
+				rs.close();
+			}
+			if(pst!=null) {
+				pst.close();
+			}
+			if(conn!=null) {
+				conn.close();
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	} // end of close
+	
+	public ArrayList<MonitoringVO> selectMonitoring(String driver_id) {
+		ArrayList<MonitoringVO> selectMonitoring = new ArrayList<>();
+
+		try {
+			connect();
+			String sql = "select * from t_monitoring where driver_id = ?";
+			pst = conn.prepareStatement(sql);
+			pst.setString(1, driver_id);
+
+			rs = pst.executeQuery();
+
+			while (rs.next()) {
+				int monitoring_seq = rs.getInt("MONITORING_SEQ");
+				int heartrate = rs.getInt("HEART_RATE");
+				int o2 = rs.getInt("O2_SATURATION");
+				int temp = rs.getInt("TEMPERATURE");
+				String regdate = rs.getString("REG_DATE");
+				
+				MonitoringVO mvo = new MonitoringVO(driver_id, monitoring_seq, heartrate, o2, temp, regdate);
+				selectMonitoring.add(mvo);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return selectMonitoring;
+	}
+	
+}
--- 27.8Hz/src/main/java/com/model/MonitoringVO.java
+++ 27.8Hz/src/main/java/com/model/MonitoringVO.java
@@ -3,14 +3,16 @@ package com.model;
 public class MonitoringVO {
 
 	private String driver_id; // ID
+	private int monitoring_seq;
 	private int heartrate; // 심박수
 	private int o2; // 산소포화도
 	private int temp; // 체온
-	private int regdate; // 수집 날짜
+	private String regdate; // 수집 날짜
 	
-	public MonitoringVO(String driver_id, int heartrate, int o2, int temp, int regdate) {
+	public MonitoringVO(String driver_id, int monitoring_seq, int heartrate, int o2, int temp, String regdate) {
 		super();
 		this.driver_id = driver_id;
+		this.monitoring_seq = monitoring_seq;
 		this.heartrate = heartrate;
 		this.o2 = o2;
 		this.temp = temp;
@@ -20,6 +22,10 @@ public class MonitoringVO {
 	
 	public String getDriver_id() {
 		return driver_id;
+	}
+	
+	public int getMonitoring_seq() {
+		return monitoring_seq;
 	}
 
 	public int getHeartrate() {
@@ -31,7 +37,7 @@ public class MonitoringVO {
 	public int getTemp() {
 		return temp;
 	}
-	public int getRegdate() {
+	public String getRegdate() {
 		return regdate;
 	}
 	
